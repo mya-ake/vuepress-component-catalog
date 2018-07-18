8c99df9d1e3e9c896226b1526388caadf26cdc50
mya-ake <make.app.ts@gmail.com>
2018-07-19 00:14:36 +0900

    試しに BaseButton コンポーネント作成

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
     <h1>Use vuepress as component catalog</h1>
+
+    <div>
+      <base-button>sample button</base-button>
+    </div>
   </div>
 </template>
 
 <script>
+import { BaseButton } from '@/components';
+
 export default {
   name: 'app',
+
+  components: {
+    BaseButton,
+  },
 };
 </script>
 
---
+++ src/components/BaseButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="base-button" v-on:click="handleClick">
+    <slot/>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'base-button',
+
+  methods: {
+    handleClick(evt) {
+      this.$emit('click', evt);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.base-button {
+  border: solid 1px #333;
+  border-radius: 3px;
+  padding: 4px 8px 5px;
+  font-size: 1rem;
+  color: #333;
+  cursor: pointer;
+}
+</style>
